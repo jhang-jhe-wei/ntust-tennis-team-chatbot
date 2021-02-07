6666781f30940edb9774c9a789aa63949cf4643f
wells <jhang0912407249@gmail.com>
2021-02-07 21:25:24 +0800

    add kamiliff setting

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,16 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+/* kamiliff default behavior */
+window.addEventListener("liff_ready", function (event) {
+    register_kamiliff_submit();
+});
+
+window.addEventListener("liff_submit", function (event) {
+    var json = JSON.stringify(event.detail.data);
+    var url = event.detail.url;
+    var method = event.detail.method;
+    var request_text = method + " " + url + "\n" + json;
+    liff_send_text_message(request_text);
+});
