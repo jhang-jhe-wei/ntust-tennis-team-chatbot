26f529849f58bf55d4d751377b53ed9315dbfff4
wells <jhang0912407249@gmail.com>
2021-02-20 15:04:47 +0800

    Add comment in routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,10 @@ Rails.application.routes.draw do
   get 'liff_entry', to: 'liff#entry'
   get 'liff_entry/:base64', to: 'liff#entry'
   post 'liff_route', to: 'liff#route'
+  #使用者註冊
   get 'notify',to: 'chatbot#notify',as: 'notify'
   get 'sign_up',to: 'chatbot#sign_up',as: 'sign_up'
-  get '個人資料',to: 'chatbot#profile'
   get '個人資料:*data',to: 'chatbot#user_setup'
+  # 個人資料 
+  get '個人資料',to: 'chatbot#profile'
 end
