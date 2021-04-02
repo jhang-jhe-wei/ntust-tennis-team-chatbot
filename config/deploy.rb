4fbc589968f7d38c8664e76646444b9636867085
wells <jhang0912407249@gmail.com>
2021-04-03 00:27:27 +0800

    add npm config

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,10 @@ append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 set :passenger_restart_with_touch, true
 # Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :default_env, {
+   PATH: '$HOME/.npm-packages/bin/:$PATH',
+   NODE_ENVIRONMENT: 'production'
+}
 
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
